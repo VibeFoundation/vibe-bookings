1f0ef187a958331f5d885c494b96b56bd986651f
Muhammad Amin Saffari Taheri <ma.saffaritaheri@gamil.com>
2023-05-30 14:25:16 +0330

    make the app simpler and use moonrepo as monorepo manager

---
+++ apps/client/vite.config.ts
@@ -0,0 +1,19 @@
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import { fileURLToPath } from "node:url";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  server: {
+    port: 3000,
+  },
+  preview: {
+    port: 3000,
+  },
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+});
