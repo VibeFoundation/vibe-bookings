64e3781447049f5ec45287476adb458fa42b91a4
Muhammad Amin Saffari Taheri <ma.saffaritaheri@gamil.com>
2023-06-03 14:31:00 +0330

    check env variables

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { App } from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { App } from "./App";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+console.log(process.env);
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
 );
