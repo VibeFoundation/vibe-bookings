1b1a5eeaead5431f86448a903c9db051f8781e7a
Muhammad Amin Saffari Taheri <ma.saffaritaheri@gmail.com>
2025-06-07 17:04:48 +0330

    fix: biome config

--- src/api.ts
+++ src/api.ts
@@ -1,6 +1,6 @@
 import {
-  createStartAPIHandler,
-  defaultAPIFileRouteHandler,
-} from '@tanstack/react-start/api'
+	createStartAPIHandler,
+	defaultAPIFileRouteHandler,
+} from "@tanstack/react-start/api";
 
-export default createStartAPIHandler(defaultAPIFileRouteHandler)
+export default createStartAPIHandler(defaultAPIFileRouteHandler);
