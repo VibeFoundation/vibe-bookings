49cf7d6ecc5381f30ef90a54a8434b8ff4db265d
Muhammad Amin Saffari Taheri <ma.saffaritaheri@gmail.com>
2025-06-15 18:36:07 +0330

    feat: add paraglide.

--- app.config.ts
+++ app.config.ts
@@ -1,29 +1,35 @@
-import { defineConfig } from '@tanstack/react-start/config'
-import viteTsConfigPaths from 'vite-tsconfig-paths'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "@tanstack/react-start/config";
+import viteTsConfigPaths from "vite-tsconfig-paths";
+import tailwindcss from "@tailwindcss/vite";
+import { paraglideVitePlugin } from "@inlang/paraglide-js";
 
-import { wrapVinxiConfigWithSentry } from '@sentry/tanstackstart-react'
+import { wrapVinxiConfigWithSentry } from "@sentry/tanstackstart-react";
 
 const config = defineConfig({
-  tsr: {
-    appDirectory: 'src',
-  },
-  vite: {
-    plugins: [
-      // this is the plugin that enables path aliases
-      viteTsConfigPaths({
-        projects: ['./tsconfig.json'],
-      }),
-      tailwindcss(),
-    ],
-  },
-})
+	tsr: {
+		appDirectory: "src",
+	},
+	vite: {
+		plugins: [
+			paraglideVitePlugin({
+				project: "./project.inlang",
+				outdir: "./src/paraglide",
+				strategy: ["preferredLanguage", "baseLocale"],
+			}),
+			// this is the plugin that enables path aliases
+			viteTsConfigPaths({
+				projects: ["./tsconfig.json"],
+			}),
+			tailwindcss(),
+		],
+	},
+});
 
 export default wrapVinxiConfigWithSentry(config, {
-  org: process.env.VITE_SENTRY_ORG,
-  project: process.env.VITE_SENTRY_PROJECT,
-  authToken: process.env.SENTRY_AUTH_TOKEN,
-  // Only print logs for uploading source maps in CI
-  // Set to `true` to suppress logs
-  silent: !process.env.CI,
-})
+	org: process.env.VITE_SENTRY_ORG,
+	project: process.env.VITE_SENTRY_PROJECT,
+	authToken: process.env.SENTRY_AUTH_TOKEN,
+	// Only print logs for uploading source maps in CI
+	// Set to `true` to suppress logs
+	silent: !process.env.CI,
+});
