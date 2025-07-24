e9a2d92f2c26c89e6b0ba15a13078410577e8f15
Muhammad Amin Saffari Taheri <ma.saffaritaheri@gmail.com>
2025-07-25 02:58:49 +0330

    feat: add paraglide.

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import { paraglideVitePlugin } from "@inlang/paraglide-js";
 import { defineConfig } from "vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteTsConfigPaths from "vite-tsconfig-paths";
@@ -7,22 +8,27 @@ import react from "@vitejs/plugin-react";
 import { wrapVinxiConfigWithSentry } from "@sentry/tanstackstart-react";
 
 const config = defineConfig({
-  plugins: [
-    // this is the plugin that enables path aliases
-    viteTsConfigPaths({
-      projects: ["./tsconfig.json"],
-    }),
-    tailwindcss(),
-    tanstackStart({ customViteReactPlugin: true, spa: {enabled: true} }),
-    react(),
-  ],
+	plugins: [
+		paraglideVitePlugin({
+			project: "./project.inlang",
+			outdir: "./src/paraglide",
+			strategy: ["baseLocale"],
+		}),
+		// this is the plugin that enables path aliases
+		viteTsConfigPaths({
+			projects: ["./tsconfig.json"],
+		}),
+		tailwindcss(),
+		tanstackStart({ customViteReactPlugin: true, spa: { enabled: true } }),
+		react(),
+	],
 });
 
 export default wrapVinxiConfigWithSentry(config, {
-  org: process.env.VITE_SENTRY_ORG,
-  project: process.env.VITE_SENTRY_PROJECT,
-  authToken: process.env.SENTRY_AUTH_TOKEN,
-  // Only print logs for uploading source maps in CI
-  // Set to `true` to suppress logs
-  silent: !process.env.CI,
+	org: process.env.VITE_SENTRY_ORG,
+	project: process.env.VITE_SENTRY_PROJECT,
+	authToken: process.env.SENTRY_AUTH_TOKEN,
+	// Only print logs for uploading source maps in CI
+	// Set to `true` to suppress logs
+	silent: !process.env.CI,
 });
